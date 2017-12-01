de2ff40cb22244ce2e2c6b947be024af7091bf91
etomaselli <etomaselli199@gmail.com>
2017-12-01 17:04:08 +0100

    Mapped workspace owner role in geoserver

--- aac.provider/oauth2-aac/src/main/java/org/geoserver/security/oauth2/AACOAuthAuthenticationFilter.java
+++ aac.provider/oauth2-aac/src/main/java/org/geoserver/security/oauth2/AACOAuthAuthenticationFilter.java
@@ -11,19 +11,35 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
+import java.util.logging.Level;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.geoserver.security.GeoServerRoleService;
+import org.geoserver.security.GeoServerRoleStore;
+import org.geoserver.security.GeoServerSecurityManager;
 import org.geoserver.security.config.SecurityNamedServiceConfig;
+import org.geoserver.security.impl.DataAccessRuleDAO;
 import org.geoserver.security.impl.GeoServerRole;
+import org.geoserver.web.GeoServerApplication;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 /**
  * @author Alessio Fabiani, GeoSolutions S.A.S.
@@ -31,6 +47,7 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
  */
 public class AACOAuthAuthenticationFilter extends GeoServerOAuthAuthenticationFilter {
 	OAuth2RestOperations oauth2RestTemplate;
+	private static final String WS_OWNER = "ROLE_WS_OWNER";
 
     public AACOAuthAuthenticationFilter(SecurityNamedServiceConfig config,
             RemoteTokenServices tokenServices,
@@ -39,6 +56,88 @@ public class AACOAuthAuthenticationFilter extends GeoServerOAuthAuthenticationFi
         super(config, tokenServices, oauth2SecurityConfiguration, oauth2RestTemplate);
         
         this.oauth2RestTemplate = oauth2RestTemplate;
+    }
+    
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String cacheKey = authenticateFromCache(this, (HttpServletRequest) request,
+                (HttpServletResponse) response);
+
+        // Search for an access_token on the request (simulating SSO)
+        String accessToken = request.getParameter("access_token");
+
+        OAuth2AccessToken token = restTemplate.getOAuth2ClientContext().getAccessToken();
+
+        if (accessToken != null && token != null && !token.getValue().equals(accessToken)) {
+            restTemplate.getOAuth2ClientContext().setAccessToken(null);
+        }
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        
+        /*
+         * This cookie works only locally, when accessing the GeoServer GUI and on the same domain.
+         * For remote access you need to logout from the GeoServer GUI.
+         */
+        final String gnCookie = getGeoNodeCookieValue(httpRequest);
+
+        final Authentication authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+        final Collection<? extends GrantedAuthority> authorities = (authentication != null
+                ? authentication.getAuthorities() : null);
+
+        if (accessToken == null && gnCookie == null && 
+                (authentication != null && (authentication instanceof PreAuthenticatedAuthenticationToken) &&  
+                !(authorities.size() == 1 && authorities.contains(GeoServerRole.ANONYMOUS_ROLE)))) {
+            final AccessTokenRequest accessTokenRequest = restTemplate.getOAuth2ClientContext()
+                    .getAccessTokenRequest();
+            if (accessTokenRequest != null && accessTokenRequest.getStateKey() != null) {
+                restTemplate.getOAuth2ClientContext()
+                        .removePreservedState(accessTokenRequest.getStateKey());
+            }
+        }
+
+        if (accessToken != null || authentication == null || (authentication != null
+                && authorities.size() == 1 && authorities.contains(GeoServerRole.ANONYMOUS_ROLE))) {
+
+            doAuthenticate((HttpServletRequest) request, (HttpServletResponse) response);
+
+            Authentication postAuthentication = authentication;
+            if (postAuthentication != null && cacheKey != null) {
+                if (cacheAuthentication(postAuthentication, (HttpServletRequest) request)) {
+                    getSecurityManager().getAuthenticationCache().put(getName(), cacheKey,
+                            postAuthentication);
+                }
+            }
+        }
+
+        chain.doFilter(request, response);
+	}
+	
+	private String getGeoNodeCookieValue(HttpServletRequest request) {
+        if (LOGGER.isLoggable(Level.FINE)) {
+            LOGGER.fine("Inspecting the http request looking for the GeoNode Session ID.");
+        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Found " + cookies.length + " cookies!");
+            }
+            for (Cookie c : cookies) {
+                if (GEONODE_COOKIE_NAME.equals(c.getName())) {
+                    if (LOGGER.isLoggable(Level.FINE)) {
+                        LOGGER.fine("Found GeoNode cookie: " + c.getValue());
+                    }
+                    return c.getValue();
+                }
+            }
+        } else {
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Found no cookies!");
+            }
+        }
+
+        return null;
     }
     
     @Override
@@ -66,17 +165,54 @@ public class AACOAuthAuthenticationFilter extends GeoServerOAuthAuthenticationFi
     	
     	if (roles != null) {
     		for (AACRole role : roles) {
+    			//normal user
+    			if (role.getRole().equals(AACRole.USER) && roles.size() == 1) {
+    				break;
+    			}
+    			//user is the provider of sco.geoserver
     			if (role.getRole().equals(AACRole.PROVIDER) && role.getContext() != null && role.getContext().equals(((AACOAuth2FilterConfig) filterConfig).getApiManagerDomain())) {
     				gsRoles.add(GeoServerRole.ADMIN_ROLE);
+    			} else if (role.getRole().startsWith(((AACOAuth2FilterConfig) filterConfig).getRolePrefix())
+    					&& role.getContext() != null && role.getContext().equals(((AACOAuth2FilterConfig) filterConfig).getApiManagerDomain())
+    					&& role.getScope().equalsIgnoreCase(AACRole.RoleScope.APPLICATION.toString())) {
+    				//user is the owner of a workspace in sco.geoserver
+    				String wsName = role.getRole().substring(((AACOAuth2FilterConfig) filterConfig).getRolePrefix().length());
+    				System.out.println("workspace: "+wsName);
+    				
+    				GeoServerRole wsOwner = new GeoServerRole(WS_OWNER);
+
+					GeoServerRoleStore store = getSecurityManager().getActiveRoleService().createStore();
+					store.removeRole(wsOwner);
+					
+    				wsOwner.getProperties().setProperty("ws_name", wsName); //set name of the workspace owned by user as role property
+    				wsOwner.setUserName(principal);
+    				if (getSecurityManager().getActiveRoleService().canCreateStore()) {
+    					store.addRole(wsOwner);
+    					//store.associateRoleToUser(wsOwner, principal);
+    					store.store();
+    				}
+    				gsRoles.add(wsOwner);
     			}
     		}
     	}
     	
-        //return super.getRoles(request, principal);
+    	//TODO policy (via REST): cache rules, check that role exists for WS
+    	//rule format: <workspace>.<layer>.[r|w|a]
+    	//json to add rule: {"rule": {"@resource": "<workspace>.<layer>.[r|w|a]","text": "role1,role2,..."}}
+    	
+    	DataAccessRuleDAO dao = getSecurityManager().getDataAccessRuleDAO();
+    	//dao.
+    	SortedSet<GeoServerRole> roleServiceRoles = getSecurityManager().getActiveRoleService().getRoles();
+    	for (GeoServerRole role : roleServiceRoles) {
+    		System.out.println("auth: "+role.getAuthority());
+    	}
+    	
     	System.out.println("roles returned from getRoles: "+gsRoles);
         return gsRoles;
     }
-    
+
+
+
 	private static class AACRole {
 		//{"id": 21,"scope": "system","role": "ROLE_ADMIN","context": null,"authority": "ROLE_ADMIN"}
 		//{"id": 21,"scope": "system","role": "ROLE_USER","context": null,"authority": "ROLE__USER"}
@@ -87,6 +223,10 @@ public class AACOAuthAuthenticationFilter extends GeoServerOAuthAuthenticationFi
 		private String context;
 		private String authority;
 		private static final String PROVIDER = "ROLE_PROVIDER";
+		private static final String USER = "ROLE_USER";
+		public enum RoleScope {
+			SYSTEM, APPLICATION, TENANT, USER
+		}
 		
 		public int getId() {
 			return id;
