c51489524a8bd23d67ad0e83d3fad805dbbb4363
AlbanaCelepija <acelepija@fbk.eu>
2017-12-13 08:26:25 +0100

    import(Example)scenario example for geographic info being showed using wps/wms services

---
+++ examples/openlayers/script_playgo.js
@@ -0,0 +1,271 @@
+
+var types=['all','bike','bus','train','walk'];
+
+function enableAuthToken (name){
+  var chkbWso2 = (document.getElementById(name) ? document.getElementById(name).checked : false);
+  var layer = (chkbWso2 ? wso2_layers[name] : layers[name]);
+  map[name].getLayers().setAt(1, layer);
+} 
+var selectedType='all';
+function loadData(type){
+  selectedType=type;
+  var layername='playgo_'+type;
+  var name='imagewms';
+  map[name].getLayers().setAt(1, layers[type+'_hm']);
+  for(var k=0;k<types.length;k++){
+    document.getElementById('lbl_'+types[k]).style.display='none';
+    document.getElementById(type+'_chkb').checked=false;
+  }
+  document.getElementById('lbl_'+type).style.display='block';
+  loadInfo(type);
+}
+
+function loadInfo(type){
+    var name='imagewms';
+    if(document.getElementById(type+'_chkb').checked==true){
+      map[name].getLayers().getArray()[2].setSource(sources[type]);
+    }
+    else
+      map[name].getLayers().getArray()[2].setSource(null);
+  }
+
+function loadFilter(value){
+    var name='imagewms';
+    console.log(sources[selectedType]);
+    console.log(sources[selectedType].getProperties());
+    sources[selectedType].updateParams({'CQL_FILTER':"validity = '"+value+"'"});
+    map[name].getLayers().getArray()[1].getSource().updateParams({'CQL_FILTER':"validity = '"+value+"'"});
+    //sources[selectedType]['params']['CQL_FILTER']="validity = 'valid'";
+}
+
+var style = new ol.style.Style({
+  text: new ol.style.Text({
+    font: 'bold 11px "Open Sans", "Arial Unicode MS", "sans-serif"',
+    placement: 'line',
+    fill: new ol.style.Fill({
+      color: 'white'
+    })
+  })
+});
+//sources
+var sources={};
+var layers={};
+// A WMS Geoserver layer
+sources['all'] =  new ol.source.ImageWMS({
+  url: geoserverUrl+'/wms',
+  params: {'LAYERS': 'topp:OGRGeoJSON_0'},
+  ratio: 1,
+  serverType: 'geoserver'
+});
+
+layers['all_hm'] =     new ol.layer.Image({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_ti'},
+          ratio: 1,
+          serverType: 'geoserver'
+  })
+});
+
+sources['bike'] =  new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_bike'},
+          ratio: 1,
+          serverType: 'geoserver'
+});
+layers['bike_hm'] =     new ol.layer.Image({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_bike','Styles':'topp:play_ti'},
+          ratio: 1,
+          serverType: 'geoserver'
+  })
+});
+
+sources['bus'] =   new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_bus'},
+          ratio: 1,
+          serverType: 'geoserver'
+});
+layers['bus_hm'] =     new ol.layer.Image({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_bus','Styles':'topp:play_ti'},
+          ratio: 1,
+          serverType: 'geoserver'
+  })
+});
+
+sources['walk'] =   new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_walk'},
+          ratio: 1,
+          serverType: 'geoserver'
+});
+layers['walk_hm'] =     new ol.layer.Image({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_walk','Styles':'topp:play_ti'},
+          ratio: 1,
+          serverType: 'geoserver'
+  })
+});
+
+sources['train'] =     new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_train'},
+          ratio: 1,
+          serverType: 'geoserver'
+});
+layers['train_hm'] =     new ol.layer.Image({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:playgo_train','Styles':'topp:play_ti'},
+          ratio: 1,
+          serverType: 'geoserver'
+  })
+});
+
+layers['all'] =     new ol.layer.Image({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.ImageWMS({
+          url: geoserverUrl+'/wms',
+          params: {'LAYERS': 'topp:OGRGeoJSON_0'},
+          ratio: 1,
+          serverType: 'geoserver'
+  })
+});
+sources['valid'] =   new ol.source.ImageWMS({
+  url: geoserverUrl+'/wms',
+  params: {'LAYERS': 'topp:playgo_walk','CQL_FILTER':"validity = 'valid'"},
+  ratio: 1,
+  serverType: 'geoserver'
+});
+
+layers['tilewcs'] =   new ol.layer.Tile({
+  //extent: [-13884991, 2870341, -7455066, 6338219],
+  source: new ol.source.TileWMS({
+          url: geoserverUrl+'/wcs?service=wcs&version=1.1.0&request=GetCapabilities',
+          params: {
+                  'LAYERS': 'medford:bikelanes,medford:bikelanes_original',
+          },
+          serverType: 'geoserver',
+  })
+});
+var vectorSource =   new ol.source.Vector({
+  format: new ol.format.GeoJSON(),
+  url: function(extent) {
+          return geoserverUrl+'/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=topp:OGRGeoJSON&outputFormat=application/json';
+  },
+  strategy: ol.loadingstrategy.bbox
+});
+layers['play_style'] = new ol.layer.Vector({
+  source: vectorSource,
+  //style: function(feature) {
+  //  style.getText().setText(feature.get('freeTracki'));
+  //  return style;
+  //}
+});
+
+// base layers
+var tile = new ol.layer.Tile({
+  source: new ol.source.OSM(),
+  name: 'OpenStreetMap'
+});
+var layerStamenWater = new ol.layer.Tile({
+  source: new ol.source.Stamen({
+          layer: 'watercolor'
+  }),
+  name: 'Watercolor'
+});
+var layerStamenTerrain = new ol.layer.Tile({
+  source: new ol.source.Stamen({
+          layer: 'terrain'
+  }),
+  name: 'Terrain'
+});
+var layerStm = new ol.layer.Group({
+  layers: [layerStamenWater, layerStamenTerrain],
+  name: 'Stamen Group'
+});
+var raster = new ol.layer.Tile({
+  source: new ol.source.Stamen({
+          layer: 'toner'
+  })
+});
+
+layers['vector_kml_wms'] = new ol.layer.Vector({
+  source: new ol.source.Vector({
+          url:    geoserverUrl+'/wms?service=WMS&version=1.0.0&request=GetMap&layers=topp:states&format=kml&width=200&height=200&bbox=-124.73142200000001, 24.955967,-66.969849, 49.371735',
+          //url:  geoserverUrl+'/wfs?service=WFS&version=1.0.0&request=GetFeature&typename=topp:states&outputFormat=kml',
+          format: new ol.format.KML({
+                  extractStyles: false
+          })
+  })
+});
+
+
+var mousePositionControl = new ol.control.MousePosition({
+  coordinateFormat: ol.coordinate.createStringXY(4),
+  undefinedHTML: '&nbsp;'
+});
+var scaleLineControl = new ol.control.ScaleLine();
+var zoomslider = new ol.control.ZoomSlider();
+var controls = ol.control.defaults().extend([
+          mousePositionControl,
+          scaleLineControl,
+          zoomslider
+]);
+13676724
+var transform=ol.proj.transform([10.887451171875,45.91389958711688], 'EPSG:4326', 'EPSG:3857');
+var zoom = 10;
+
+var layers_geo_wms =            [tile,layers['all_hm'],layers['all']];
+var layers_play_style =         [tile,layers['play_style']];
+var layers_geo_wcs =            [tile,layers['tilewcs']];
+var layers_geo_wms_kml =        [tile,layers['vector_kml_wms']];
+
+var map={};
+map['imagewms'] = new ol.Map({
+  controls: controls,
+  layers: layers_geo_wms,
+  target: "map['imagewms']",
+  view: new ol.View({
+          center: transform,
+          zoom:zoom,
+  })
+});
+loadData('all');
+document.getElementById('all').checked=true;
+map['tilewcs'] = new ol.Map({
+  controls: ol.control.defaults().extend([
+    new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      undefinedHTML: '&nbsp;'
+    }),
+    new ol.control.ScaleLine(),
+    zoomslider
+]),
+  layers: layers_geo_wcs,
+  target: "map['tilewcs']",
+  view: new ol.View({
+          center: ol.proj.transform([-122.875595,42.326515], 'EPSG:4326', 'EPSG:3857'),
+          zoom:12,
+  })
+});
+
+map['play_style'] = new ol.Map({
+  controls: controls,
+  layers: layers_play_style,
+  target: "map['play_style']",
+  view: new ol.View({
+          center: transform,
+          zoom:zoom,
+  })
+});
